3b71622d248a8f426d15c987249dc4e67d2b76b9
astel <astel.develop@gmail.com>
2017-11-30 16:40:25 +0300

    PostConstruct init

--- src/main/java/quater/TerminatorQuoter.java
+++ src/main/java/quater/TerminatorQuoter.java
@@ -2,16 +2,28 @@ package quater;
 
 import lombok.Setter;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quater {
 
     @InjectRandomInt(min = 1, max = 4)
     private int repeat;
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
 
     @Override
     public void sayQuote() {
         for( int i = 0; i < repeat; i++) {
             System.out.println(message);
         }
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Phase 2");
+        System.out.println(repeat);
     }
 
     @Setter
